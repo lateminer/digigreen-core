c1b9b391b762f6ca0bdb1c3bb3a167ed1238e76a
lateminer <9951982+lateminer@users.noreply.github.com>
2025-01-28 23:10:54 +0100

    build: Update depends as of Blackcoin 13.2

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -1,20 +1,31 @@
-packages:=boost openssl libevent zeromq
-native_packages := native_ccache
+packages:=boost openssl libevent
 
-qt_native_packages = native_protobuf
-qt_packages = qrencode protobuf zlib
+protobuf_native_packages = native_protobuf
+protobuf_packages = protobuf
 
-qt_linux_packages:=qt expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libX11 xextproto libXext xtrans
+qrencode_packages = qrencode
 
+qt_linux_packages:=qt expat libxcb xcb_proto libXau xproto freetype fontconfig libxkbcommon
+
+qt_android_packages=qt
 qt_darwin_packages=qt
 qt_mingw32_packages=qt
 
 wallet_packages=bdb
 
+zmq_packages=zeromq
+
 upnp_packages=miniupnpc
 
-darwin_native_packages = native_biplist native_ds_store native_mac_alias
+darwin_native_packages = native_ds_store native_mac_alias
+
+$(host_arch)_$(host_os)_native_packages += native_b2
 
 ifneq ($(build_os),darwin)
-darwin_native_packages += native_cctools native_cdrkit native_libdmg-hfsplus
+darwin_native_packages += native_cctools native_libtapi native_libdmg-hfsplus
+
+ifeq ($(strip $(FORCE_USE_SYSTEM_CLANG)),)
+darwin_native_packages+= native_clang
+endif
+
 endif
